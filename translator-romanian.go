11e6b69e9d494464ef3f177d3070f113dd280538
cristian-sima <cs25g13@soton.ac.uk>
2015-11-25 11:56:25 +0200

    Improved the Romanian translator

--- translator-romanian.go
+++ translator-romanian.go
@@ -9,6 +9,7 @@ type RomanianTranslator struct {
 	translator
 }
 
+//
 // Translate transforms the error messages from result set to an array of
 // human messages
 func (translator RomanianTranslator) Translate(results *ValidationResults) map[string][]string {
@@ -18,42 +19,42 @@ func (translator RomanianTranslator) Translate(results *ValidationResults) map[s
 // TranslateRule translates a method into a english human message
 func (translator RomanianTranslator) TranslateRule(method string, options string) string {
 
-	message := "Validarea pentru regula [" + method + "] nu a reusit!"
+	message := "Validarea pentru regula [" + method + "] nu a reuşit!"
 
 	switch method {
 	case "accepted":
-		message = "Trebuie sa fie 'yes', 'on', true, or 1. Permite valori numberice si text"
+		message = "Acest câmp trebuie să fie 'yes', 'on', true, sau 1. Sunt permise valori numerice sau text."
 	case "url", "email", "ipv4", "ipv6", "date", "ip":
-		message = "Trebuie sa fie un " + strings.ToUpper(method) + " valid"
+		message = "Câmpul trebuie sa fie un " + strings.ToUpper(method) + " valid."
 	case "regex":
-		message = "Trebuie sa se potriveasca acestei expresii regulate: " + options
+		message = "Trebuie să se potrivească acestei expresii regulate: " + options
 	case "between":
 		betweenMessage := translator.getMessageBetween(options)
-		message = "Lungimea trebuie sa fie intre " + betweenMessage + " de caractere (limitele nu sunt permise)"
+		message = "Lungimea trebuie să fie între " + betweenMessage + " de caractere (fară intervale)"
 	case "between_inclusive":
 		betweenMessage := translator.getMessageBetween(options)
-		message = "Lungimea trebuie sa fie intre " + betweenMessage + " de caractere (limitele sunt permise)"
+		message = "Lungimea trebuie să fie între " + betweenMessage + " de caractere (inclusiv intervalele)"
 	case "digits_between":
 		betweenMessage := translator.getMessageBetween(options)
-		message = "Campul trebuie sa fie de tip numeric si trebuie sa fie intre " + betweenMessage + " cifre"
+		message = "Câmpul trebuie să fie de tip numeric şi trebuie să fie între " + betweenMessage + " cifre"
 	case "min":
 		message = "Lungimea minimă permisă este de " + options
 	case "max":
 		message = "Lungimea maximă permisă este de " + options
 	case "len":
-		message = "Trebuie sa aiba exact " + options + " cifre"
+		message = "Trebuie să aibă exact " + options + " cifre"
 	case "alpha":
 		message = "Acesta trebuie să fie în întregime caractere alfabetice"
 	case "alpha_dash":
 		message = "Aceasta poate avea caractere alfanumerice, precum și liniuțe de subliniere și cratime."
 	case "alpha_num":
-		message = "Acesta trebuie să aiba în întregime caractere alfanumerice"
+		message = "Acesta trebuie să aibă în întregime caractere alfanumerice"
 	case "digits":
-		message = "Trebuie sa fie un numar si sa aiba un number de " + options + " de cifre"
+		message = "Trebuie să fie un număr şi să aibă un number de " + options + " de cifre"
 	case "required":
-		message = "Câmpul în curs de validare trebuie sa fie prezent. Este acceptat orice tip. Notă:  optiunile nu funcționează atunci când încearcă să valideze structs, deoarece nu este posibil să se știe dacă valorile nule sunt zero, deoarece acestea nu sunt stabilite, sau pentru că ar trebui să fie de fapt de zero"
+		message = "Câmpul în curs de validare trebuie sa fie prezent. Sunt acceptate toate tipurile. Notă: opţiunile nu funcționează atunci când se încearcă să se valideze structs, deoarece nu este posibil să se știe dacă valorile nule sunt zero, deoarece acestea nu sunt stabilite, sau pentru că ar trebui să fie de fapt de zero"
 	case "full_name":
-		message = "Câmpul trebuie să conțină caractere alfanumerice sau puncte sau spații"
+		message = "Câmpul trebuie să conțină caractere alfanumerice, puncte sau spații"
 	}
 	return message
 }
